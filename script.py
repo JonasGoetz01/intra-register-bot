72cd642f9ebb168cd4ee2d5d28ab8076080943c9
Jonas Götz <jonas.goetz01@web.de>
2024-05-24 12:27:44 +0000

    working docker file and changed script with info of availability

--- script.py
+++ script.py
@@ -13,6 +13,9 @@ chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--remote-debugging-port=9222')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--window-size=1920x1080')
 
 # Set the path to the ChromeDriver
 service = Service('/usr/local/bin/chromedriver')
@@ -21,23 +24,25 @@ service = Service('/usr/local/bin/chromedriver')
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
 # Open a website
-driver.get("https://auth.42.fr/auth/realms/students-42/protocol/openid-connect/auth?client_id=intra&redirect_uri=https%3A%2F%2Fprofile.intra.42.fr%2Fusers%2Fauth%2Fkeycloak_student%2Fcallback&response_type=code&state=18e6e5cf87aed485fd73aacc6da26dcc0451a85f2bf7d0bc")
+driver.get("https://intra.42.fr")
 
 usernameField = driver.find_element(By.ID, "username")
 passwordField = driver.find_element(By.ID, "password")
 
 # Find an element by its name attribute and type something
-usernameField.send_keys(os.getenv(os.getenv("USERNAME")))
-passwordField.send_keys(os.getenv(os.getenv("PASSWORD")))
+usernameField.send_keys(os.getenv("USERNAME"))
+passwordField.send_keys(os.getenv("PASSWORD"))
 login_button = driver.find_element(By.ID, "kc-login")
 login_button.click()
 
 # Wait for some time to let the search results load
-time.sleep(5)
+# time.sleep(5)
 
 # Find a specific element by its tag name and print its text
-results = driver.find_element(By.TAG_NAME, "h3")
-print(results.text)
+availability = driver.find_element(By.CLASS_NAME, "user-poste-status")
+print(availability.text)
+host = driver.find_element(By.CLASS_NAME, "user-poste-infos")
+print(host.text)
 
 # Close the browser
 driver.quit()
